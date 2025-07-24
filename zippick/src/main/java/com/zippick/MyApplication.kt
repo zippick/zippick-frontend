d1c681f5386b6765bdb95809a69ffed772bd7392
김현지 <chik1y@kookmin.ac.kr>
2025-07-24 15:21:20 +0900

    [FEAT] 폴더명 수정

--- zippick/src/main/java/com/zippick/MyApplication.kt
+++ zippick/src/main/java/com/zippick/MyApplication.kt
@@ -9,7 +9,7 @@ class MyApplication : Application() {
 
     companion object {
         val QUERY = "travel"
-        val API_KEY = "079dac74a5f94ebdb990ecf61c8854b7"
+        val API_KEY = ""
         val BASE_URL = "https://newsapi.org"
         val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
 
