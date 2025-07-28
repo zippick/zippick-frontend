15b26d567714e45b8f26126adeb242b1a1525c61
DESKTOP-SUEF0JA\KOSA <aswe0409@gmail.com>
2025-07-28 10:45:27 +0900

    feat: 로그인 API 연동

--- Zippick/app/src/main/java/com/example/zippick/MainActivity.kt
+++ Zippick/app/src/main/java/com/example/zippick/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.zippick.network.TokenManager
 import com.example.zippick.ui.composable.MainScreenWithBottomNav
 import com.example.zippick.ui.theme.AndroidLabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        TokenManager.init(this)
 
         enableEdgeToEdge()
 
---
+++ Zippick/app/src/main/java/com/example/zippick/ui/model/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.example.zippick.ui.model
+
+data class LoginRequest(
+    val loginId: String,
+    val password: String
+)
